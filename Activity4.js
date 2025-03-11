c7a17f60ad759af85efacc9d3b380d20f418854c
kielmoriscalub <kielmoriscalub9@gmail.com>
2025-03-11 17:04:28 +0800

    Add files via upload

---
+++ Activity4.js
@@ -0,0 +1,14 @@
+//Comparison or Relational Operators
+
+
+let firstFavNumb = 7;  
+let secondFavNumb = 10; 
+
+console.log("Greater than:", firstFavNumb > secondFavNumb);
+console.log("Less than:", firstFavNumb < secondFavNumb);
+console.log("Greater than or equal to:", firstFavNumb >= secondFavNumb);
+console.log("Less than or equal to:", firstFavNumb <= secondFavNumb);
+console.log("Strict equality (===):", firstFavNumb === secondFavNumb);
+console.log("Loose equality (==):", firstFavNumb == secondFavNumb);
+console.log("Strict inequality (!==):", firstFavNumb !== secondFavNumb);
+console.log("Loose inequality (!=):", firstFavNumb != secondFavNumb);
