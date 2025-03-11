36b25cabadb60a87e829775be99641ce836de098
kielmoriscalub <kielmoriscalub9@gmail.com>
2025-03-11 16:57:07 +0800

    Add files via upload

---
+++ Activity2.js
@@ -0,0 +1,18 @@
+// Declare favorite numbers
+let firstFavNum = 7; 
+let secondFavNum = 3; 
+
+
+let addition = firstFavNum + secondFavNum;
+let subtraction = firstFavNum - secondFavNum;
+let multiplication = firstFavNum * secondFavNum;
+let division = firstFavNum / secondFavNum;
+let modulus = firstFavNum % secondFavNum;
+let exponentiation = firstFavNum ** secondFavNum; 
+
+console.log("Addition: " + addition);
+console.log("Subtraction: " + subtraction);
+console.log("Multiplication: " + multiplication);
+console.log("Division: " + division);
+console.log("Modulus: " + modulus);
+console.log("Exponentiation: " + exponentiation);
