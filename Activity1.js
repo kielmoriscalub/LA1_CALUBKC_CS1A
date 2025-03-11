b684fc2c1441d681e2f4af932f03484a7acc1923
kielmoriscalub <kielmoriscalub9@gmail.com>
2025-03-11 16:51:32 +0800

    Add files via upload

---
+++ Activity1.js
@@ -0,0 +1,11 @@
+//Variables
+
+let name = "Your Name";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Your Gender";
+let twitterHandle = "@YourTwitterHandle";
+
+console.log("Name:", name);
+console.log("What do you want to become in life:", whatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Twitter Handle:", twitterHandle);
