2ca4f6d4a4c6b49ea84f63f5c59da931d4892f06
kielmoriscalub <kielmoriscalub9@gmail.com>
2025-03-11 17:09:47 +0800

    Add files via upload

---
+++ Activity5.js
@@ -0,0 +1,21 @@
+//strings
+let favActorFirstName = "Thomas"; 
+let favActorLastName = "Middleditch";
+
+
+let fullName = favActorFirstName + " " + favActorLastName;
+
+
+let uppercase = fullName.toUpperCase();
+
+
+let message = "My favorite actor is " + uppercase + ".";
+
+
+message += " His best show is Silicon Valley.";
+
+console.log("First Name: " + favActorFirstName);
+console.log("Last Name: " + favActorLastName);
+console.log("Full Name: " + fullName);
+console.log("Uppercase Name: " + uppercase);
+console.log("Message: " + message);
