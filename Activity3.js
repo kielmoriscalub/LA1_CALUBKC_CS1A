2e1b06faeee1035bb7e9e3f6849c2b0d42c251b2
kielmoriscalub <kielmoriscalub9@gmail.com>
2025-03-11 17:00:59 +0800

    Add files via upload

---
+++ Activity3.js
@@ -0,0 +1,12 @@
+//Booleans
+
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+
+let favNumb = 7;
+
+favNumb = undefined;
+
+console.log("Is JavaScript a programming language?", isJsProgrammingLanguage);
+console.log("Is JavaScript hard?", isJsHard);
+console.log("Favorite Number:", favNumb);
